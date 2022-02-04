65b22e05a7ebe5f3ed1e60625f667c5ce1f6fbd8
Marco LAMOTHE <mlamothe@e2r2p12.clusters.42paris.fr>
2022-02-04 15:53:05 +0100

    cd change bien OLDPWD et PWD

--- builtins/ft_pwd.c
+++ builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: mlamothe <mlamothe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 13:15:30 by mlamothe          #+#    #+#             */
-/*   Updated: 2022/01/27 16:08:15 by mlamothe         ###   ########.fr       */
+/*   Updated: 2022/02/04 15:51:34 by mlamothe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@
 int	ft_pwd(t_mini *mini)
 {
 	char	*dir;
-	char	*buf;
 
-	buf = NULL;
-	dir = getcwd(buf, 0);
+	dir = NULL;
+	dir = getcwd(dir, 0);
 	if (dir)
 	{
 		write(1, dir, ft_strlen(dir));
