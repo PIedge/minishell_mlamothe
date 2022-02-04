b122493d0054a5011795e6730b43c99b6feb5531
tmerrien <tmerrien@student.42.fr>
2022-02-04 15:02:10 +0100

    dgf

--- builtins/ft_export.c
+++ builtins/ft_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlamothe <mlamothe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tmerrien <tmerrien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 01:46:28 by mlamothe          #+#    #+#             */
-/*   Updated: 2022/02/04 11:26:27 by mlamothe         ###   ########.fr       */
+/*   Updated: 2022/02/04 14:57:33 by tmerrien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,37 @@ int	ft_env_export(t_mini *mini)
 	return (1);
 }
 
+int	check_name_var_val(char *name)
+{
+	int	i;
+
+	i = 0;
+	if ((name[i] < 'a' || name[i] > 'z') && \
+		(name[i] < 'A' || name [i] > 'Z') && name[i] != '_')
+		return (0);
+	++i;
+	while (name[i])
+	{
+		if ((name[i] < 'a' || name[i] > 'z') && \
+			(name[i] < 'A' || name [i] > 'Z') && \
+				name[i] != '_' && (name[i] < '0' || name[i] > '9'))
+			return (i);
+		++i;
+	}
+	return (1);
+}
+
 int	ft_export(t_cmd *cmd, t_mini *mini)
 {
 	char	**new_env;
 	int		alrd_here;
+	int		cc;
 
 	if (!cmd->cm_argv[1])
 		return (ft_env_export(mini));
+	cc = check_name_var_val(cmd->cm_argv[1]);
+	if (cc == 0 || cmd->cm_argv[1][cc] != '=')
+		return (set_error(mini, 1, 1, NULL));
 	alrd_here = get_var_name_end(cmd->cm_argv[1], mini);
 	if (alrd_here != -1)
 	{
